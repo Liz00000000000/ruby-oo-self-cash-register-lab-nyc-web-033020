b344b1e39096d65b17df620dc4ac7d87db3a9a35
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-04-01 18:37:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,9 +14,19 @@ class CashRegister
     end
   end
 
+  def apply_discount
+    if discount != 0
+      self.total = total * ((100 - discount)/100)
+      puts "After the discount, the total comes to #{self.total}."
+    else
+      puts "There is no discount to apply."
+  end
+
+  end
+
  def void_last_transaction
    self.total = self.total - self.last_transaction
- end 
+ end
 
 
 end
