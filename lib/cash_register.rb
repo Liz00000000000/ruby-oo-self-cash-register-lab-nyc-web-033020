e42bb34b77e6d329838c8be1cb82019fef529a90
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-04-01 16:00:28 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,24 @@
 class CashRegister
   attr_accessor :register
-  def initialize(register = @total)
+  attr_writer :discount
+  def initialize(register = @total, discount = 0)
     @register = register
-    @total = 0 
+    @total = 0
+    @discount = discount
   end
+
+  def total
+    returns total 
+  end
+
+  def cash_register_with_discount
+  end
+
+  def checkout(discount = 0)
+  end
+
+#  def apply_discount
+#    total * 0.8
+#  end
+
 end
